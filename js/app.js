2a91dd20213ac2ab6703a304d9bb872bfc812319
mcswork22-code <mcswork2022@gmail.com>
2026-07-05 21:13:39 +0900

    Ver1.2 DateTime

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("Aviation Dashboard by mckoi");
+function updateDateTime(){
+
+    const now = new Date();
+
+    const week=["日","月","火","水","木","金","土"];
+
+    const text =
+        now.getFullYear()+" / "+
+        String(now.getMonth()+1).padStart(2,"0")+" / "+
+        String(now.getDate()).padStart(2,"0")+
+        " ("+week[now.getDay()]+")<br>"+
+        String(now.getHours()).padStart(2,"0")+":"+
+        String(now.getMinutes()).padStart(2,"0")+":"+
+        String(now.getSeconds()).padStart(2,"0");
+
+    document.getElementById("datetime").innerHTML=text;
+
+}
+
+updateDateTime();
+
+setInterval(updateDateTime,1000);
